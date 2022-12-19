eed8c9a528844862ae5d43a63cbf2784b0227524
0x41-li <90697156+0x41-li@users.noreply.github.com>
2022-12-19 21:04:04 +0100

    Making the header sticky

--- src/sections/Header.vue
+++ src/sections/Header.vue
@@ -4,9 +4,11 @@ import LogoSVG from "../assets/logo.svg";
 
 <template>
   <header
-    class="[box-shadow:_inset_0_-1px_rgba(5,_30,_86,_0.16)]"
+    class="[box-shadow:_inset_0_-1px_rgba(5,_30,_86,_0.16)] sticky top-0 left-0 bg-[#fdf8fd] z-50"
   >
-    <div class="p-4 flex justify-between 2xl:max-w-[1440px] 2xl:mx-auto xl:px-[150px]">
+    <div
+      class="p-4 flex justify-between 2xl:max-w-[1440px] 2xl:mx-auto xl:px-[150px]"
+    >
       <a class="block" href="/">
         <LogoSVG />
       </a>
