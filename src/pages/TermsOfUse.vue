c5140115deb3775e960cfac91208d603812da720
0x41-li <90697156+0x41-li@users.noreply.github.com>
2022-12-21 14:41:43 +0100

    Terms of use content completed

--- src/pages/TermsOfUse.vue
+++ src/pages/TermsOfUse.vue
@@ -228,6 +228,318 @@
           this Agreement.
         </li>
       </ol>
+
+      <h3 class="text-darkBlue text-base font-bold lg:text-2xl mt-4 lg:mt-8">
+        5. Restrictions
+      </h3>
+      <ol class="pl-8">
+        <li
+          class="relative text-darkBlue text-base font-normal pl-12 lg:text-2xl before:content-['5.1.'] before:absolute before:left-0 before:top-0"
+        >
+          Except as expressly set out in this Agreement or as permitted by any
+          applicable law, you undertake:
+          <ol class="">
+            <li
+              class="relative text-darkBlue text-base font-normal pl-16 lg:text-2xl before:content-['5.1.1.'] before:absolute before:left-0 before:top-0"
+            >
+              not to reproduce, copy, modify, adapt, translate, publish,
+              display, communicate, transmit, sell, exploit or use the whole or
+              any part of any Service, our APP or any of the contents therein
+              for any commercial or other purposes;
+            </li>
+            <li
+              class="relative text-darkBlue text-base font-normal pl-16 lg:text-2xl before:content-['5.1.2.'] before:absolute before:left-0 before:top-0"
+            >
+              not to disassemble, decompile, reverse-engineer or create
+              derivative works based on the whole or any part of the source code
+              of our APP nor attempt to do any such thing, or to reproduce,
+              display or otherwise provide access to the Services, our APP or
+              any of the contents therein, including but not limited to framing,
+              mirroring, linking, spidering, scraping or any other technological
+              means;
+            </li>
+            <li
+              class="relative text-darkBlue text-base font-normal pl-16 lg:text-2xl before:content-['5.1.3.'] before:absolute before:left-0 before:top-0"
+            >
+              not to provide or otherwise make available our APP in whole or in
+              part (including but not limited to program listings, object and
+              source program listings, object code and source code), in any form
+              to any person without prior written consent from us;
+            </li>
+            <li
+              class="relative text-darkBlue text-base font-normal pl-16 lg:text-2xl before:content-['5.1.4.'] before:absolute before:left-0 before:top-0"
+            >
+              to include our copyright notice on all entire and partial copies
+              you make of our APP on any medium;
+            </li>
+            <li
+              class="relative text-darkBlue text-base font-normal pl-16 lg:text-2xl before:content-['5.1.5.'] before:absolute before:left-0 before:top-0"
+            >
+              to comply with all applicable technology control or export laws
+              and regulations; and
+            </li>
+            <li
+              class="relative text-darkBlue text-base font-normal pl-16 lg:text-2xl before:content-['5.1.6.'] before:absolute before:left-0 before:top-0"
+            >
+              not to disrupt, disable, or otherwise impair the proper working of
+              the Services, our APP or our servers, such as through hacking,
+              cyber-attacks (including but not limited to denial-of-service
+              attacks), tampering or reprogramming.
+            </li>
+          </ol>
+        </li>
+      </ol>
+
+      <h3 class="text-darkBlue text-base font-bold lg:text-2xl mt-4 lg:mt-8">
+        6. Intellectual Property Rights
+      </h3>
+      <ol class="pl-8">
+        <li
+          class="relative text-darkBlue text-base font-normal pl-12 lg:text-2xl before:content-['6.1.'] before:absolute before:left-0 before:top-0"
+        >
+          You acknowledge that all intellectual property rights in our APP
+          anywhere in the world belong to us, that rights in our APP are
+          licensed (not sold) to you, and that you have no rights in, or to, our
+          APP other than the right to use them in accordance with these Terms.
+        </li>
+        <li
+          class="relative text-darkBlue text-base font-normal pl-12 lg:text-2xl before:content-['6.2.'] before:absolute before:left-0 before:top-0"
+        >
+          Any intellectual property rights in content uploaded by you to our APP
+          shall continue to belong to you or their respective owners. You agree
+          that you grant us a royalty-free and non-exclusive license to use,
+          reproduce, publish, and display such intellectual property rights for
+          the purposes of performing the Services, promotional purposes,
+          internal administrative purposes and any other purposes set out in
+          these Terms, including for the purpose of improving the Services and
+          our responses to users of the APP.
+        </li>
+        <li
+          class="relative text-darkBlue text-base font-normal pl-12 lg:text-2xl before:content-['6.3.'] before:absolute before:left-0 before:top-0"
+        >
+          You acknowledge that you have no right to have access to our APP in
+          source code form.
+        </li>
+        <li
+          class="relative text-darkBlue text-base font-normal pl-12 lg:text-2xl before:content-['6.4.'] before:absolute before:left-0 before:top-0"
+        >
+          You must not use any part of the content on our APP for commercial
+          purposes not specified on our APP without obtaining a license to do so
+          from us or our licensors.
+        </li>
+      </ol>
+
+      <h3 class="text-darkBlue text-base font-bold lg:text-2xl mt-4 lg:mt-8">
+        7. Warranties
+      </h3>
+      <ol class="pl-8">
+        <li
+          class="relative text-darkBlue text-base font-normal pl-12 lg:text-2xl before:content-['7.1.'] before:absolute before:left-0 before:top-0"
+        >
+          While we make all efforts to maintain the accuracy of the information
+          on our APP, we provide the Services, APP and all Related Content on an
+          “as is” and “as available” basis, unless otherwise specified in
+          writing. We make no representations or warranties of any kind, express
+          or implied, as to the operation of any of the foregoing, unless
+          otherwise specified in writing.
+        </li>
+        <li
+          class="relative text-darkBlue text-base font-normal pl-12 lg:text-2xl before:content-['7.2.'] before:absolute before:left-0 before:top-0"
+        >
+          To the full extent permissible by law, we disclaim all warranties,
+          express or implied, relating to our APP or any Services, including but
+          not limited to implied warranties of merchantability and fitness for a
+          particular purpose. We do not warrant that the Services, our APP, the
+          Related Content, or electronic communications sent by us are free of
+          viruses or other harmful components.
+        </li>
+      </ol>
+
+      <h3 class="text-darkBlue text-base font-bold lg:text-2xl mt-4 lg:mt-8">
+        8. Limitation of Liability
+      </h3>
+      <ol class="pl-8">
+        <li
+          class="relative text-darkBlue text-base font-normal pl-12 lg:text-2xl before:content-['8.1.'] before:absolute before:left-0 before:top-0"
+        >
+          We are not liable for the completeness, accuracy or correctness of any
+          information uploaded on our APP and any Related Content. You expressly
+          agree that your use of the Services and our APP, is at your sole risk.
+        </li>
+        <li
+          class="relative text-darkBlue text-base font-normal pl-12 lg:text-2xl before:content-['8.2.'] before:absolute before:left-0 before:top-0"
+        >
+          You agree not to use the Services, our APP and the Related Content for
+          any re-sale purposes, and we have no liability to you, whether in
+          contract, tort (including negligence), breach of statutory duty, or
+          otherwise, arising under or in connection with these Terms (including
+          but not limited to the use of, or inability to use, the Services, our
+          APP or any other website or software) for:
+          <ol class="">
+            <li
+              class="relative text-darkBlue text-base font-normal pl-16 lg:text-2xl before:content-['8.2.1.'] before:absolute before:left-0 before:top-0"
+            >
+              loss of profits, sales, business, or revenue;
+            </li>
+            <li
+              class="relative text-darkBlue text-base font-normal pl-16 lg:text-2xl before:content-['8.2.2.'] before:absolute before:left-0 before:top-0"
+            >
+              business interruption;
+            </li>
+            <li
+              class="relative text-darkBlue text-base font-normal pl-16 lg:text-2xl before:content-['8.2.3.'] before:absolute before:left-0 before:top-0"
+            >
+              loss of anticipated savings;
+            </li>
+            <li
+              class="relative text-darkBlue text-base font-normal pl-16 lg:text-2xl before:content-['8.2.4.'] before:absolute before:left-0 before:top-0"
+            >
+              loss or corruption of data or information;
+            </li>
+            <li
+              class="relative text-darkBlue text-base font-normal pl-16 lg:text-2xl before:content-['8.2.5.'] before:absolute before:left-0 before:top-0"
+            >
+              loss of business opportunity, goodwill or reputation; or
+            </li>
+            <li
+              class="relative text-darkBlue text-base font-normal pl-16 lg:text-2xl before:content-['8.2.6.'] before:absolute before:left-0 before:top-0"
+            >
+              any other indirect or consequential loss or damage.
+            </li>
+          </ol>
+        </li>
+        <li
+          class="relative text-darkBlue text-base font-normal pl-12 lg:text-2xl before:content-['8.3.'] before:absolute before:left-0 before:top-0"
+        >
+          Nothing in these Terms shall limit or exclude our liability for:
+          <ol class="">
+            <li
+              class="relative text-darkBlue text-base font-normal pl-16 lg:text-2xl before:content-['8.3.1.'] before:absolute before:left-0 before:top-0"
+            >
+              death or personal injury resulting from our negligence;
+            </li>
+            <li
+              class="relative text-darkBlue text-base font-normal pl-16 lg:text-2xl before:content-['8.3.2.'] before:absolute before:left-0 before:top-0"
+            >
+              fraud; and/or
+            </li>
+            <li
+              class="relative text-darkBlue text-base font-normal pl-16 lg:text-2xl before:content-['8.3.3.'] before:absolute before:left-0 before:top-0"
+            >
+              any other matter in respect of which we are prohibited under
+              applicable law from limiting or excluding our liability.
+            </li>
+          </ol>
+        </li>
+        <li
+          class="relative text-darkBlue text-base font-normal pl-12 lg:text-2xl before:content-['8.4.'] before:absolute before:left-0 before:top-0"
+        >
+          Our APP is not intended to serve a record-keeping function and we
+          shall not be liable for any loss of data or content.
+        </li>
+        <li
+          class="relative text-darkBlue text-base font-normal pl-12 lg:text-2xl before:content-['8.5.'] before:absolute before:left-0 before:top-0"
+        >
+          These Terms set out the full extent of our obligations and liabilities
+          in respect of the supply of the Services and our APP. Except as
+          expressly stated in these Terms, there are no conditions, warranties,
+          representations, or other terms, express or implied, that are binding
+          on us. Any condition, warranty, representation, or other term
+          concerning the supply of the Services and our APP which might
+          otherwise be implied into, or incorporated in, these Terms whether by
+          statute, common law or otherwise, is excluded to the fullest extent
+          permitted by law.
+        </li>
+        <li
+          class="relative text-darkBlue text-base font-normal pl-12 lg:text-2xl before:content-['8.6.'] before:absolute before:left-0 before:top-0"
+        >
+          We will be not held responsible for any delay or failure to perform or
+          comply with our obligations under these terms and conditions when the
+          delay or failure arises from any cause which is beyond our reasonable
+          control.
+        </li>
+      </ol>
+
+      <h3 class="text-darkBlue text-base font-bold lg:text-2xl mt-4 lg:mt-8">
+        9. Indemnity
+      </h3>
+      <p class="text-darkBlue text-base font-normal lg:text-2xl mt-8 lg:mt-16">
+        You agree to indemnify and hold us, and our respective directors,
+        officers, employees, agents and representatives, independent
+        contractors, licensees, successors and assigns harmless from and against
+        all claims, losses, expenses, damages and costs (including but not
+        limited to direct, incidental, consequential, exemplary and indirect
+        damages), and reasonable legal fees, resulting from or arising out of
+        your act, default or omission, whether in your use of our APP, Services,
+        and/or any websites or software in relation thereto or otherwise, and
+        whether in respect of your breach of these Terms or any laws or
+        regulations or otherwise.
+      </p>
+
+      <h3 class="text-darkBlue text-base font-bold lg:text-2xl mt-4 lg:mt-8">
+        10. Other Important Terms
+      </h3>
+      <ol class="pl-8">
+        <li
+          class="relative text-darkBlue text-base font-normal pl-16 lg:text-2xl before:content-['10.1.'] before:absolute before:left-0 before:top-0"
+        >
+          We may transfer our rights and obligations under these Terms to
+          another organization, but this will not affect your rights or
+          obligations under these Terms.
+        </li>
+        <li
+          class="relative text-darkBlue text-base font-normal pl-16 lg:text-2xl before:content-['10.2.'] before:absolute before:left-0 before:top-0"
+        >
+          You may only transfer your rights or your obligations under these
+          Terms to another person if we agree in writing.
+        </li>
+        <li
+          class="relative text-darkBlue text-base font-normal pl-16 lg:text-2xl before:content-['10.3.'] before:absolute before:left-0 before:top-0"
+        >
+          No joint venture, partnership or agency or employment relationship has
+          arisen by reason of these Terms.
+        </li>
+        <li
+          class="relative text-darkBlue text-base font-normal pl-16 lg:text-2xl before:content-['10.4.'] before:absolute before:left-0 before:top-0"
+        >
+          These Terms and any document expressly referred to in it constitutes
+          the entire agreement between us regarding their subject matter, and
+          supersedes and extinguishes all previous agreements, promises,
+          assurances, warranties, representations, and understandings between
+          us, whether written or oral, relating to that subject matter.
+        </li>
+        <li
+          class="relative text-darkBlue text-base font-normal pl-16 lg:text-2xl before:content-['10.5.'] before:absolute before:left-0 before:top-0"
+        >
+          If we fail to insist that you perform any of your obligations under
+          these Terms, or if we do not enforce our rights against you, or if we
+          delay in doing so, that will not mean that we have waived our rights
+          against you and will not mean that you do not have to comply with
+          those obligations. If we do waive a default by you, we will only do so
+          in writing, and that will not mean that we will automatically waive
+          any later default by you.
+        </li>
+        <li
+          class="relative text-darkBlue text-base font-normal pl-16 lg:text-2xl before:content-['10.6.'] before:absolute before:left-0 before:top-0"
+        >
+          Each of the conditions of these Terms operates separately. If any
+          court or competent authority decides that any of them are unlawful or
+          unenforceable, the remaining conditions will remain in full force and
+          effect.
+        </li>
+        <li
+          class="relative text-darkBlue text-base font-normal pl-16 lg:text-2xl before:content-['10.7.'] before:absolute before:left-0 before:top-0"
+        >
+          These Terms, its subject matter and its formation, and any other
+          disputes or claims in connection therewith, are governed by the law of
+          Turkey. In the event of any such disputes or claims in connection with
+          these Terms, you agree to first engage in good faith discussions with
+          us to resolve such dispute or claim. If such dispute or claim is not
+          resolved within sixty (60) days, we both irrevocably submit to the
+          exclusive jurisdiction of the courts of Turkey.
+        </li>
+      </ol>
+
       <!--  -->
     </div>
   </section>
